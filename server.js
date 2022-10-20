bf17e10e20e7fbe21780f7ed228825a9782e9fdc
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2022-10-20 13:41:32 +0700

    adding mysql support

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql')
+
+let con  = mysql.createConnection({
+    host: "localhost",
+    user: "user",
+    password: "pass",
+    database: "database",
+    port: 3306
+})
+
+con.connect((err) => {
+    if(err) throw err
+    console.log("Connected!s")
+})
 
 app.use(express.json())
 
