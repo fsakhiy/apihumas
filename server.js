580d520fdc3bc24596f894ee95b5d51bf319f03f
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2022-10-20 14:38:17 +0700

    adding some major feature

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const mysql = require('mysql')
 
 let con  = mysql.createConnection({
     host: "localhost",
-    user: "user",
-    password: "pass",
-    database: "database",
+    user: "root",
+    password: "12345678",
+    database: 'test',
     port: 3306
 })
 
@@ -24,12 +24,34 @@ app.get('/', (req, res) => {
 
 app.get('/alumni/:criteria', (req, res) => {
     let criteria = req.params.criteria
-    if(criteria == "all") {
-        con.query("select * from alumni", (err, result, fields) => {
-            if(err) throw err
-            console.log(result)
-        })
+    let sql
+    if(criteria === "all") {
+        sql = "select * from alumni"
+    } else {
+        criteria = parseInt(criteria)
+        sql = `select * from alumni where id=${criteria}`
     }
+    
+    con.query(sql, (err, result, fields) => {
+        if (err) throw err
+        res.send(result)
+    })
+})
+
+app.post('/alumni', (req, res) => {
+    let { nama, tahunlulus, jurusan, status  } = req.body
+
+    // let sql = `insert into test value (${req.body.age}, "${req.body.name}")`
+    // con.query(sql, (err, result) => {
+    //     if (err) throw err;
+    //     res.send('record inserted')
+    // })
+
+    let sql = `insert into alumni(nama, tahunLulus, jurusan, status) value ("${req.body.nama}", ${req.body.tahunlulus}, ${req.body.jurusan}, "${req.body.status}")`
+    con.query(sql, (err, result) => {
+        if(err) throw err
+        res.send('record inserted')
+    })
 })
 
 app.listen(port, () => {console.log(`server is running on port ${port}`)})
