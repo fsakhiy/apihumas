8c4255d1892829faa3eaefe9a407727b61868bf6
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2022-10-24 14:03:10 +0700

    simplifying code

--- server.js
+++ server.js
@@ -75,27 +75,36 @@ app.get('/', (req, res) => {
 //     })
 // })
 
-function checkcriteria(data, criteria) {
-    let sql
-    if(criteria === "all") {
-        sql = `select * from ${data}`
-    } else if(typeof criteria == "number"){
-        criteria = parseInt(criteria)
-        sql = `select * from ${data} where id=${criteria}`
-    } else {
-        throw ReferenceError("data not found")
-    }
-    con.query(sql, (err, result) => {
-        if (err) throw err
-        return result
-    })
-}
+// function checkcriteria(data, criteria) {
+//     let sql
+//     if(criteria === "all") {
+//         sql = `select * from ${data}`
+//     } else if(typeof criteria == "number"){
+//         criteria = parseInt(criteria)
+//         sql = `select * from ${data} where id=${criteria}`
+//     } else {
+//         throw ReferenceError("data not found")
+//     }
+//     con.query(sql, (err, result) => {
+//         if (err) throw err
+//         return result
+//     })
+// }
 
 app.get('/:data/:criteria', (req, res) => {
     const data = req.params.data
     const criteria = req.params.criteria
-    let sql
-    res.send(checkcriteria(data, criteria))
+    let sql = `select * from ${data}`
+
+    if(criteria != "all") {
+        criteria = parseInt(criteria)
+        sql = `select * from ${data} where id=${criteria}`
+    }
+
+    con.query(sql, (err, result) => {
+        if (err) throw err
+        res.send(result)
+    })
 })
 
 //--------------------- POST -----------------
